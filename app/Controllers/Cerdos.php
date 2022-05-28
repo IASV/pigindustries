6ec2b203aa5c03fc0af8d141916d889595bb6998
keduro12 <kduvangarcia@unicesar.edu.co>
2022-05-27 23:28:34 -0500

    cerdos 2.0

--- app/Controllers/Cerdos.php
+++ app/Controllers/Cerdos.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->session = \Config\Services::session();
+		$this->request = \Config\Services::request();
+    }
+    
+
     public function index()
     {
-        return view('welcome_message');
+        //return view('welcome_message');
+        print_r("Añadir cerdos");
     }
 }
